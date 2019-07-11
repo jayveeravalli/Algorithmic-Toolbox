21f11ba77955b89f9ba327dd09cc5f043d686d1d
VEERAVALLI, SAI (sv477p) <sv477p@att.com>
2019-07-10 21:34:58 -0400

    car refuelling

--- src/CarFueling.java
+++ src/CarFueling.java
@@ -2,8 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class CarFueling {
-    static int computeMinRefills(int dist, int tank, int[] stops) {
-        return -1;
+
+    private static int computeMinRefills(int dist, int tank, int[] stops) {
+
+        int remaining = tank;
+        int prev = -1;
+        int i = 0;
+        int refills = 0;
+
+        while ( i < stops.length) {
+
+            if (prev == -1) {
+                if (stops[i] > tank) {
+                    return -1;
+                }
+
+                remaining = tank - stops[i];
+                prev = i;
+                i++;
+                continue;
+            }
+
+            if (stops[i] - stops[prev] > tank) {
+                return -1;
+            }
+
+            remaining = remaining - (stops[i] - stops[prev]);
+
+            if (remaining == 0) {
+                remaining = tank;
+                prev = i;
+                i++;
+                refills++;
+                continue;
+            }
+
+            if (remaining < 0 ) {
+                remaining = tank;
+                refills++;
+                continue;
+            }
+
+            prev = i;
+            i++;
+
+
+        }
+
+        if (remaining == tank && dist - stops[stops.length-1] >remaining) {
+            return -1;
+        }
+
+        if (dist - stops[stops.length-1] <= remaining) {
+            return refills;
+        } else {
+            return refills+1;
+        }
     }
 
     public static void main(String[] args) {

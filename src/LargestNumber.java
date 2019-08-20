fa1d73dba7f5e491ca28ba66e90162ada57fbd54
VEERAVALLI, SAI (sv477p) <sv477p@att.com>
2019-08-19 23:27:58 -0400

    Added solution to largest number

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -1,64 +1,42 @@
 import java.util.*;
 
 public class LargestNumber {
-    private static String largestNumber(int[] a) {
+    private static String largestNumber(String[] a) {
         StringBuilder result = new StringBuilder();
         boolean[] visited = new boolean[a.length];
-        int L = a.length;
-        int max = Integer.MIN_VALUE;
 
-        while (L >= 0) {
+        for (int i = 0; i < a.length; i++) {
             int pos = 0;
-            int removed_elem = -1;
+            int index = 0;
+            String max = "0";
+
             while (pos < a.length) {
-                if (!visited[pos] && isGreater(max, a[pos])) {
+                if (!visited[pos] && greaterThanMax(max, a[pos])){
                     max = a[pos];
-                    removed_elem = pos;
+                    index = pos;
                 }
                 pos++;
             }
-            result.append(a[removed_elem]);
-            visited[removed_elem] = true;
-            L--;
+            result.append(max);
+            visited[index] = true;
         }
 
         return result.toString();
     }
 
-    private static boolean isGreater(int curr_max, int element) {
-        if (curr_max < 0) {
-            return true;
-        }
+    private static boolean greaterThanMax(String max, String value) {
+        String a = max+value;
+        String b = value+max;
 
-        int curr_max_ = first_digit(curr_max);
-        int element_ = first_digit(element);
-
-        if (curr_max_ > element_) {
-            return false;
-        } else if(curr_max_ < element_) {
-            return true;
-        } else {
-            if (curr_max > element) {
-                return false
-            } else {
-
-            }
-        }
-    }
-
-    private static int first_digit(int n) {
-        while (n/10 > 0) {
-            n = n / 10;
-        }
-        return n;
+        return Integer.parseInt(a) < Integer.parseInt(b);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] a = new int[n];
+        String[] a = new String[n];
         for (int i = 0; i < n; i++) {
-            a[i] = scanner.nextInt();
+            a[i] = scanner.next();
         }
         System.out.println(largestNumber(a));
     }
